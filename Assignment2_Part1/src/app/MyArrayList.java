f14e17f3e3210b1a17c76c174570f4346c4ff6b3
JashanGill <JashanGill@localhost>
2022-03-14 20:25:18 -0600

    The MyArrayList is Implemented but the Iterator is left

---
+++ Assignment2_Part1/src/app/MyArrayList.java
@@ -0,0 +1,239 @@
+/**
+ * 
+ */
+package app;
+
+import java.lang.reflect.Array;
+
+import utilities.Iterator;
+import utilities.ListADT;
+
+/**
+ * @author JashanGill
+ *
+ */
+public class MyArrayList<E> implements ListADT<E> {
+	//the actual size of the arrayList
+	private int size;
+	//The length of the array that contains the ArrayList
+	private int arrayLength;
+	//The array that contains the ArrayList
+	private  Object[] array;
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * constructor
+	 */
+	public MyArrayList() {
+	
+		this.size = 0;
+		this.arrayLength = 10;
+		this.array = new Object[arrayLength];
+	}
+	/**
+	 * This function returns the size of the arrrayList
+	 */
+	@Override
+	public int size() {
+		
+		return size;
+	}
+	/**
+	 * This methood clears the whole arrayList
+	 */
+	@Override
+	public void clear() {
+		
+		array = null;
+		size = 0;
+	}
+	/**
+	 * This method counts the elements in the List
+	 * @return the count of elements
+	 */
+	private int elementCount()
+	{
+		int count = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] != null) {
+				count++;
+			}
+		}
+		return count;
+	}
+	/**
+	 * This Method adds the new element to the array
+	 */
+	@Override
+	public boolean add(int index, E toAdd) throws NullPointerException, IndexOutOfBoundsException {
+		int indexDiff = 0;
+		
+		//checks if the index to which the element is being added is less than the length of the Array
+		if(this.size!=this.arrayLength && index<this.arrayLength)
+		{
+			//if YES, It adds the element
+			array[index] = toAdd;
+		}
+		else
+		{
+			//If No, It creates a new Array with size that can contain 10 more elements
+			Object[] tempArr  = new Object[this.arrayLength];
+			arrayLength += 10;
+			//then we transfer all the elements from the old array to the new array by using a Temp array
+			for (int i = 0; i < array.length; i++) {
+				tempArr[i] = array[i];
+			}
+			array = new Object[arrayLength];
+			
+			for (int i = 0; i < tempArr.length; i++) {
+				array[i] = tempArr[i];
+			}
+			tempArr = null;
+			
+			//Then we add the element
+			array[index] = toAdd;	
+		}
+		//this is an additional component that updates the size of the arrayList 
+		/*
+		 * For Example:
+		 * if list has only one element at index 5 the the size of the List should be 6 not 1
+		 * and, if the list has one element at a index 3 and the new one is added at the index 6 then the size should be 7 not 5
+		 */
+		for (int i = index-1; i >= 0; i--) {
+			if (elementCount() == 1) {
+				indexDiff = index;
+				break;
+			}
+			if (array[i] != null) {
+				indexDiff = index - i - 1;
+				break;
+			}
+		}
+		this.size += indexDiff+1;
+		
+		return true;
+	}
+	/**
+	 * adds the element to the arrayList at the end of the list
+	 */
+	@Override
+	public boolean add(E toAdd) throws NullPointerException {
+		add(this.size, toAdd);
+		return true;
+	}
+
+	@Override
+	public boolean addAll(ListADT<? extends E> toAdd) throws NullPointerException {
+		
+		for (int i = 0; i < toAdd.size(); i++) {
+			add(toAdd.get(i));
+		}
+
+		return true;
+	}
+
+	@Override
+	public E get(int index) throws IndexOutOfBoundsException {
+		if(index > this.size-1) {
+			throw new IndexOutOfBoundsException();
+		}
+		Object getValue = array[index];
+		return (E)getValue;
+	}
+
+	@Override
+	public E remove(int index) throws IndexOutOfBoundsException {
+		if(index > this.size-1) {
+			throw new IndexOutOfBoundsException();
+		}
+		// this the removed element that needs to be returned by the method
+		Object getValue = array[index];
+		// deleting the element
+		array[index] = null;
+		
+		//this loop check for the last element in the list to set the size of the list to that element
+		for (int i = index-1; i >= 0; i--) {
+			if (array[i] != null) {
+				this.size = i+1;
+				break;
+			}
+		//this if will see if the element removed was the last element then set List size to zero 	
+		}if(elementCount() == 0) {
+			this.size = 0;
+		}
+		return (E)getValue;
+	}
+
+	@Override
+	public E remove(E toRemove) throws NullPointerException {
+		int indexToBeRemoved = -1;
+		for (int i = 0; i < this.size; i++) {
+			if (array[i] == toRemove) {
+				indexToBeRemoved = i;
+			}
+		}
+		if(indexToBeRemoved == -1) {
+			throw new UnsupportedOperationException();
+		}
+		return remove(indexToBeRemoved);
+	}
+
+	@Override
+	public E set(int index, E toChange) throws NullPointerException, IndexOutOfBoundsException {
+		if(toChange == null) {
+			throw new NullPointerException();
+		}
+		
+		array[index] = toChange;
+		return toChange;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		if (this.size == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public boolean contains(E toFind) throws NullPointerException {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == toFind) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public E[] toArray(E[] toHold) throws NullPointerException {
+		Object[] convertedArray = new Object[this.size];
+		for (int i = 0; i < toHold.length; i++) {
+			convertedArray[i] = (Object)toHold[i];
+		}
+		return (E[])convertedArray;
+	}
+
+	@Override
+	public Object[] toArray() {
+		Object[] convertedArray = new Object[this.size];
+		for (int i = 0; i < this.size; i++) {
+			convertedArray[i] = array[i];
+		}
+		return convertedArray;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+
+	
--- Assignment2_Part1/test/tests/MyArrayListTests.java
+++ Assignment2_Part1/test/tests/MyArrayListTests.java
@@ -9,17 +9,23 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import app.MyArrayList;
+
 /**
  * @author JashanGill
  *
  */
 class MyArrayListTests {
+	
+	MyArrayList<String> arr;
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@BeforeEach
 	void setUp() throws Exception {
+		
+		arr = new MyArrayList<String>();
 	}
 
 	/**
@@ -27,11 +33,413 @@ class MyArrayListTests {
 	 */
 	@AfterEach
 	void tearDown() throws Exception {
+		
+		arr = null;
+		
 	}
 
+
+	/**
+	 * Test method for {@link app.MyArrayList#size()}.
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testSizeZero() {
+		assertEquals(0, arr.size());
 	}
+	
+
+	/**
+	 * Test method for {@link app.MyArrayList#size()}.
+	 */
+	@Test
+	void testSizeNonZero() {
+		arr.add("A");
+		arr.add("B");
+		arr.add("C");
+		
+		assertEquals(3, arr.size());
+	}
+
+	/**
+	 * Test method for {@link app.MyArrayList#clear()}.
+	 */
+	@Test
+	void testClear() {
+		arr.add("A");
+		arr.add("B");
+		arr.add("C");
+		
+		arr.clear();
+		
+		assertEquals(0,arr.size());
+	}
+
+	/**
+	 * Test method for {@link app.MyArrayList#add(int, java.lang.Object)}.
+	 */
+	@Test
+	void testAddToBeginning() {
+		arr.add(0, "A");
+		
+		assertEquals("A", arr.get(0));
+		
+	}
+	
+	/**
+	 * Test method for {@link app.MyArrayList#add(int, java.lang.Object)}.
+	 */
+	@Test
+	void testAddToMiddle() {
+
+		arr.add(5, "A");
+		
+		assertEquals("A", arr.get(5));
+		
+	}
+
+	/**
+	 * Test method for {@link app.MyArrayList#add(java.lang.Object)}.
+	 */
+	@Test
+	void testAddE() {
+
+		arr.add("A");
+		
+		assertEquals("A", arr.get(arr.size()-1));	
+		
+	}
+
+	/**
+	 * Test method for {@link app.MyArrayList#addAll(utilities.ListADT)}.
+	 */
+	@Test
+	void testAddAll() {
+		
+		MyArrayList<String>	arr2 = new MyArrayList<String>();
+		
+		arr2.add("A");
+		arr2.add("B");
+		arr2.add("C");
+		
+		arr.add("X");
+		arr.add("Y");
+		arr.add("Z");
+		
+		arr.addAll(arr2);
+		
+		String result = "";
+		for (int i = 0;i<arr.size();i++) {
+			result += arr.get(i);
+		}
+		assertEquals("XYZABC", result);
+		
+	}
+
+	/**
+	 * Test method for {@link app.MyArrayList#get(int)}.
+	 */
+	@Test
+	void testGetFromBeginnig() {
+		arr.add("C");
+		arr.add("A");
+		arr.add("L");
+		arr.add("G");
+		arr.add("A");
+		arr.add("R");
+		arr.add("Y");
+		
+		assertEquals("C", arr.get(0));
+	}	
+	
+	/**
+	 * Test method for {@link app.MyArrayList#get(int)}.
+	 */
+	@Test
+	void testGetFromMiddle() {
+		arr.add("C");
+		arr.add("A");
+		arr.add("L");
+		arr.add("G");
+		arr.add("A");
+		arr.add("R");
+		arr.add("Y");
+		
+		assertEquals("G", arr.get(3));	
+		
+	}
+	
+	/**
+	 * Test method for {@link app.MyArrayList#get(int)}.
+	 */
+	@Test
+	void testGetFromEnd() {
+		arr.add("C");
+		arr.add("A");
+		arr.add("L");
+		arr.add("G");
+		arr.add("A");
+		arr.add("R");
+		arr.add("Y");
+		
+		assertEquals("Y", arr.get(6));	
+		
+	}
+	
+	/**
+	 * Test method for {@link app.MyArrayList#get(int)}.
+	 */
+	@Test
+	void testGetFromBadIndex() {
+		arr.add("C");
+		arr.add("A");
+		arr.add("L");
+		arr.add("G");
+		arr.add("A");
+		arr.add("R");
+		arr.add("Y");
+		
+		assertThrows(IndexOutOfBoundsException.class, ()-> {
+			arr.get(9);
+		});
+	}
+
+	/**
+	 * Test method for {@link app.MyArrayList#remove(int)}.
+	 */
+	@Test
+	void testRemoveFromBeginning() {
+		arr.add("C");
+		arr.add("A");
+		arr.add("L");
+		arr.add("G");
+		arr.add("A");
+		arr.add("R");
+		arr.add("Y");
+		
+		assertEquals("C", arr.remove(0));	
+	}
+	
+	/**
+	 * Test method for {@link app.MyArrayList#remove(int)}.
+	 */
+	@Test
+	void testRemoveFromMiddle() {
+		arr.add("C");
+		arr.add("A");
+		arr.add("L");
+		arr.add("G");
+		arr.add("A");
+		arr.add("R");
+		arr.add("Y");
+		
+		assertEquals("R", arr.remove(5));
+	}
+	
+	/**
+	 * Test method for {@link app.MyArrayList#remove(int)}.
+	 */
+	@Test
+	void testRemoveFromEnd() {
+		arr.add("C");
+		arr.add("A");
+		arr.add("L");
+		arr.add("G");
+		arr.add("A");
+		arr.add("R");
+		arr.add("Y");
+		
+		assertEquals("Y", arr.remove(6));
+	}
+	
+	/**
+	 * Test method for {@link app.MyArrayList#remove(int)}.
+	 */
+	@Test
+	void testRemoveFromBadIndex() {
+		arr.add("C");
+		arr.add("A");
+		arr.add("L");
+		arr.add("G");
+		arr.add("A");
+		arr.add("R");
+		arr.add("Y");
+		
+		assertThrows(IndexOutOfBoundsException.class, ()-> {
+			arr.remove(8);
+		});
+	}
+
+
+	/**
+	 * Test method for {@link app.MyArrayList#remove(java.lang.Object)}.
+	 */
+	@Test
+	void testRemoveGoodValue() {
+		arr.add("C");
+		arr.add("A");
+		arr.add("L");
+		arr.add("G");
+		arr.add("A");
+		arr.add("R");
+		arr.add("Y");
+		
+		assertEquals("L", arr.remove("L"));
+	}
+	
+	/**
+	 * Test method for {@link app.MyArrayList#remove(java.lang.Object)}.
+	 */
+	@Test
+	void testRemoveBadValue() {
+		arr.add("C");
+		arr.add("A");
+		arr.add("L");
+		arr.add("G");
+		arr.add("A");
+		arr.add("R");
+		arr.add("Y");
+		
+		assertThrows(UnsupportedOperationException.class, ()-> {
+			arr.remove("X");
+		});
+	}
+
+	/**
+	 * Test method for {@link app.MyArrayList#set(int, java.lang.Object)}.
+	 */
+	@Test
+	void testSetAtIndex() {
+		arr.add("C");
+		arr.add("A");
+		arr.add("L");
+		arr.add("G");
+		arr.add("A");
+		arr.add("R");
+		arr.add("Y");
+		
+		arr.set(3, "M");
+		assertEquals("M", arr.get(3));
+		
+	}
+	
+	/**
+	 * Test method for {@link app.MyArrayList#set(int, java.lang.Object)}.
+	 */
+	@Test
+	void testSetNullObject() {
+		arr.add("C");
+		arr.add("A");
+		arr.add("L");
+		arr.add("G");
+		arr.add("A");
+		arr.add("R");
+		arr.add("Y");
+		
+		assertThrows(NullPointerException.class, ()-> {
+			arr.set(8, null);
+		});
+	}
+	
+	/**
+	 * Test method for {@link app.MyArrayList#isEmpty()}.
+	 */
+	@Test
+	void testIsEmptyForEmpty() {
+		
+		if(arr.size() == 0) {
+			assertTrue(true);
+		}else {assertTrue(false);}
+		
+	}
+	
+	/**
+	 * Test method for {@link app.MyArrayList#isEmpty()}.
+	 */
+	@Test
+	void testIsEmptyForNonEmpty() {
+		
+		arr.add("A");
+		arr.add("B");
+		arr.add("C");
+		
+		assertEquals(3, arr.size());
+		
+	}
+
+	/**
+	 * Test method for {@link app.MyArrayList#contains(java.lang.Object)}.
+	 */
+	@Test
+	void testContainsGoodValue() {
+		arr.add("C");
+		arr.add("A");
+		arr.add("L");
+		arr.add("G");
+		arr.add("A");
+		arr.add("R");
+		arr.add("Y");
+		
+		assertTrue(arr.contains("L"));
+	}
+	
+	/**
+	 * Test method for {@link app.MyArrayList#contains(java.lang.Object)}.
+	 */
+	@Test
+	void testContainsBadValue() {
+		arr.add("C");
+		arr.add("A");
+		arr.add("L");
+		arr.add("G");
+		arr.add("A");
+		arr.add("R");
+		arr.add("Y");
+		
+		assertFalse(arr.contains("x"));
+	}
+	
+
+	/**
+	 * Test method for {@link app.MyArrayList#contains(java.lang.Object)}.
+	 */
+	@Test
+	void testSizeChangeAfterRemove() {
+		arr.add(0,"C");
+		arr.add(1,"A");
+		arr.add(2,"L");
+		arr.add(3,"G");
+		arr.add(4,"A");
+		arr.add(5,"R");
+		arr.add(9,"Y");
+		
+		arr.remove("Y");
+		
+		assertEquals(6, arr.size());
+	}
+	
+
+//	/**
+//	 * Test method for {@link app.MyArrayList#toArray(E[])}.
+//	 */
+//	@Test
+//	void testToArrayEArray() {
+//		fail("Not yet implemented");
+//	}
+//
+//	/**
+//	 * Test method for {@link app.MyArrayList#toArray()}.
+//	 */
+//	@Test
+//	void testToArray() {
+//		fail("Not yet implemented");
+//	}
+//
+//	/**
+//	 * Test method for {@link app.MyArrayList#iterator()}.
+//	 */
+//	@Test
+//	void testIterator() {
+//		fail("Not yet implemented");
+//	}
 
 }
